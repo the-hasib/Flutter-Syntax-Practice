00522d957ea9da4581943944802f3a7318678ed3
the-hasib <hasib.cse.uiu@gmail.com>
2025-10-05 16:07:09 +0600

    Input name and int number and calculate sum

--- User_input&output.dart
+++ User_input&output.dart
@@ -1,12 +1,19 @@
 import 'dart:io';
 
 void main() {
-  print("Enter 2 Number for Sum:");
-  print("Enter 1st Number :");
+  //intput a name 
+  stdout.write("Enter your name :");
+  var name = stdin.readLineSync();
+  stdout.write("Hello $name \n");
+
+  //input 2 int number
+  stdout.write("Enter 2 Number for Sum:\n");
+  stdout.write("Enter 1st Number :");
   int num1 = int.parse(stdin.readLineSync()!);
-  print("Enter 2nd Number:");
+  stdout.write("Enter 2nd Number:");
   int num2 = int.parse(stdin.readLineSync()!);
+
+  //sum and output 
   int sum = num1 + num2;
-  print("Result = $sum");
-  
+  stdout.write("Result = $sum");
 }
