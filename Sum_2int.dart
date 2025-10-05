62273320676be0dc3fe97432aed4149503d5c63f
the-hasib <hasib.cse.uiu@gmail.com>
2025-10-05 15:41:08 +0600

    Add sum function

---
+++ Sum_2int.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 5;
+  int b = 10;
+  int Sum = a + b;// If use second option don't need this line .
+  print ("Sum = $Sum");
+  /* Second option is 
+  Print ("Sum = ${a+b}"); */
+
+  }
+
